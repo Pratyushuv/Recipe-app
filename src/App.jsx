184e3df094c9495346b6d50813a21e2671050e99
Vidhya M Robert <vidhya2929@gmail.com>
2025-10-10 16:14:33 +0530

    feat:resolved comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
+  function handleSearch(searchword) {
+    console.log("Searched:", searchword);
+  }
   return (
     <>
       <Home />
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
 
-function SearchBar(){
+function SearchBar({onSearch}){
     const[search, setSearch] = useState("");
     function handleSubmit(e){
         e.preventDefault();
         if(!search.trim()) return;
-        console.log("Searched:",search);
+        onSearch(search);
         setSearch("");
     }
     return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Search for recipes..." value={search} onChange={(e) =>setSearch(e.target.value)} />
-                <button>Search</button>
+        <div className="flex flex-col items-center mt-8 w-full px-4">
+            <form onSubmit={handleSubmit} className="flex justify-center w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl">
+                <input className="flex-grow px-5 py-3 text-base sm:text-lg rounded-l-full border border-gray-300 caret-[rgba(114,63,95,1)] focus:outline-none bg-white" type="text" placeholder="Search for recipes..." value={search} onChange={(e) =>setSearch(e.target.value)} />
+                <button className="bg-[rgba(114,63,95,1)] hover:bg-[rgba(114,63,95,0.9)] text-white px-5 py-3 rounded-r-full font-semibold text-base sm:text-lg">Search</button>
             </form>
         </div>
     )
