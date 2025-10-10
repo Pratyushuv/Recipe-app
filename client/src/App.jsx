0e8d08179871a616f932490a8e73d72769a0147d
Pratyushuv <pratyush.u@vonnue.com>
2025-10-10 11:28:08 +0530

    react app created

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <div className="text-2xl text-amber-700">Hello world</div>;
+}
+
+export default App;
