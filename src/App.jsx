609daaf1e77e1707024600ce5046c4c65e04b863
Vidhya M Robert <vidhya2929@gmail.com>
2025-10-11 12:54:15 +0530

    fix:comments resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  function handleSearch(searchword) {
-    console.log("Searched:", searchword);
-  }
   return (
     <>
-      <Home onSearch={handleSearch} />
+      <Home />
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { RecipeCard } from "./card";
@@ -8,7 +9,7 @@ export default function Home() {
   const fetchData = async (searchword = "chicken") => {
     try {
       const res = await axios.get(
-        "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken",
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchword}`,
       );
       const recipeData = res.data.meals;
       setRecipe(recipeData);
@@ -25,8 +26,29 @@ export default function Home() {
   }, []);
 
   function handleSearchClick(searchword){
+=======
+import { useEffect } from "react"
+import axios from "axios"
+import SearchBar from "./Searchbar";
+export default function Home(){
+    //TODO:should be integrated once the UI is ready
+    const fetchData=async(searchword = "chicken")=>{
+        try{
+            const res=await axios.get(
+                `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchword}`)
+                console.log(res.data.meals)
+        }catch(err){
+        console.log("failed to fetch",err)
+    }
+};
+useEffect(()=>{
+    fetchData()
+},[])
+
+function handleSearchClick(searchword){
+    console.log("search word: ",searchword)
+>>>>>>> 9a5b042 (fix:comments resolved)
     fetchData(searchword);
-    onSearch(searchword);
 }
 
   return (
