0276377a102cfbbb8d14c958f392cfd6d1f0892c
Vidhya M Robert <vidhya2929@gmail.com>
2025-10-14 13:54:45 +0530

    feat: implemented viewRecipeDetails function

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./components/Home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import RecipeDetails from "./pages/ViewDetails";
+import Favourites from "./pages/Favourites";
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<Home />} />
+     <Route path="/favourites" element={<Favourites />} />
     <Route path="/recipe/:id" element={<RecipeDetails/>}/>
    </Routes>
    </BrowserRouter>
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,16 +1,15 @@
 import {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom"
 import RecipeCard from "../components/card";
 
 export default function Favourites(){
     const[favouriteRecipes, setFavouriteRecipes] = useState([]);
+    const navigate = useNavigate()
 
     useEffect(() => {
         const storedRecipes = JSON.parse(localStorage.getItem("fav")) || [];
         setFavouriteRecipes(storedRecipes);
     },[]);
-
-    //TODO need to show view recipe details
-    const handleViewRecipeDetails = () => {};
 
     const handleRemoveFavourites = (recipe) => {
         const updatedFav = favouriteRecipes.filter((fav) => fav.id !== recipe.id);
@@ -21,14 +20,13 @@ export default function Favourites(){
   return (
     <div className="p-5">
       <h1 className="text-3xl font-bold text-center mb-8">Your Favourites</h1>
-
       {favouriteRecipes.length > 0 ? (
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {favouriteRecipes.map((recipe) => (
             <RecipeCard
               key={recipe.id}
               {...recipe}
-              handleViewRecipeDetails={() => handleViewRecipeDetails(recipe)}
+              handleViewRecipeDetails={() => navigate(`/recipe/${recipe.id}`)}
               handleAddFavourites={() => handleRemoveFavourites(recipe)}
               isFavourite={true}
             />
